18553c04d9067245819a8dc13c7ad2f5f1a7d223
breinerpico <breinerviviescas@colegioguanenta.edu.co>
2024-03-01 08:05:35 -0500

    se realizo el ejercicio 2

---
+++ ejercicio N.2/README.md
@@ -0,0 +1,24 @@
+# ejercicio 2 taller condicional
+
+## prestamo bancario
+
+# ANALISIS
+
+variable de entrada(input)
+
+ingresos del cliente
+
+tiene deudas
+
+variables de proceso y salida(processing, storage, output)
+
+rta=aceptado el prestamo
+
+rta=rechazado el prestamo
+
+rta=rechazado el prestamo por que tiene deudas
+
+
+# DISEÑO
+
+![Diagrama de flujo](diagrama.png "Diagrama de flujo")
---
+++ ejercicio N.2/ejercicio2.py
@@ -0,0 +1,23 @@
+"ejercicio 2"
+
+# input
+
+
+ingresos=int(input("Digite sus ingresos: "))
+
+
+# processing 
+
+if ingresos<945200:
+    print("No puede hacer el prestamo ya que sus ingresos son menores a $945200")
+
+
+if ingresos>=945200:
+    deudas=int(input("Tiene alguna deuda:SI TIENE Digite (1),SI NO TIENE Digite (2): "))
+    SN=deudas
+   
+if SN==(1):
+    print("No puede hacer el prestamo ya que tiene deudas ")
+else:
+     SN==(2)
+     print("Puede hacer el presamo ya que no tiene deudas y sus ingresos son mayores a $945200")
