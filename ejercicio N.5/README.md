223b377c159ec91fefd5587a38b24492c89f2e5c
breinerpico <breinerviviescas@colegioguanenta.edu.co>
2024-03-08 08:35:05 -0500

    se realizo el ejercicio 5

---
+++ ejercicio N.5/README.md
@@ -0,0 +1,7 @@
+# ejercicio 5 taller condicional 
+
+# DISEÑO
+
+![Diagrama de flujo](diagrama.png "Diagrama de flujo")
+
+# CONSTUCCION
---
+++ ejercicio N.5/ejercicio5.py
@@ -0,0 +1,21 @@
+# input 
+
+metros_gastados=int(input("digite los metros cuadrados: "))
+
+# processing 
+
+if metros_gastados >50: 
+    costo=0 
+
+elif metros_gastados >50 and metros_gastados <=200:
+    costo=2000 * (metros_gastados - 50)
+
+else:
+    metros_gastados >200
+    costo=3000 * (metros_gastados - 50)
+
+costo_agua=10000 + costo
+
+# output
+
+print("El costo del agua es: ",costo_agua)
